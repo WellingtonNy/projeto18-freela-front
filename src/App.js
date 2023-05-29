2693795b08456db73b8dfcc031310de1145d8668
WellingtonLinux <wellingtoncpfilho@gmail.com>
2023-05-29 11:56:40 -0300

    estrutura

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import styled from "styled-components"
 
-function App() {
+
+
+import { useState } from "react"
+import { UserContext } from "./contexts/UserContext"
+
+export default function App() {
+  const [user,setUser] =useState({})
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <PagesContainer>
+      <BrowserRouter>
+      <UserContext.Provider value={ {user,setUser} }>
+        <Routes>
+          
+     
+        </Routes>
+        </UserContext.Provider>
+      </BrowserRouter>
+    </PagesContainer>
+  )
 }
 
-export default App;
+const PagesContainer = styled.main`
+  background-color: #8c11be;
+  width: calc(100vw - 50px);
+  max-height: 100vh;
+  padding: 25px;
+`
